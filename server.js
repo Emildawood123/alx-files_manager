305047c43cdd09f6a59a3e04e76e2c889ec0b698
Emildawood123 <edawood997@gmail.com>
2024-05-11 12:07:03 -0400

    massage when server run

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = Express();
 const PORT = 5000;
 app.use(router);
 app.listen(PORT, () => {
-  console.log(`listen in port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
