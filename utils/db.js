31e1a1adec74cf3af756f0a6b4ec0216fbd0d314
Emildawood123 <edawood997@gmail.com>
2024-05-11 09:00:07 -0400

    second task

---
+++ utils/db.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const port = process.env.DB_PORT || 27017;
+    const host = process.env.DB_HOST || 'localhost';
+    const dbName = process.env.DB_NAME || 'files_manager';
+    this.client = MongoClient(`mongodb://${host}:${port}/${dbName}`);
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.client.db().collection('users');
+    return collection.countDocuments();
+  }
+
+  async nbFiles() {
+    const collection = this.client.db().collection('files');
+    return collection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
