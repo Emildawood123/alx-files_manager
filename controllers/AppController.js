d51070e38de354ecf6cabdff70b1c5384faa5dee
Emildawood123 <edawood997@gmail.com>
2024-05-11 12:27:57 -0400

    delete condition

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,12 +3,10 @@ import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
-    if (redisClient.isAlive() && dbClient.isAlive()) {
-      res.status(200).send({
-        redis: redisClient.isAlive(),
-        db: dbClient.isAlive(),
-      });
-    }
+    res.status(200).send({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
   }
 
   static async getStats(req, res) {
