01a243c3abd28983f0168f86467d2ffd84c54191
Emildawood123 <edawood997@gmail.com>
2024-05-11 12:10:21 -0400

    forget process.env.Port

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ import router from './routes';
 
 const app = Express();
 
-const PORT = 5000;
-app.use(router);
+const PORT = process.env.PORT || 5000;
+app.use(Express.json());
+app.use('/', router);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
