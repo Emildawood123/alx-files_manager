a6708e1761d271398cf567c0a669ffd0b54767e6
Emildawood123 <edawood997@gmail.com>
2024-05-12 11:41:06 -0400

    last try in this session

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,6 +1,10 @@
 class FilesController {
-    static async postUpload() {
-        
+    static async postUpload(res, req) {
+        const token = req.header('X-Token')
+        const userid = await redisClient.get(`auth_${token}`);
+        if (!userid) {
+            res.status(401).send({ error: 'Unauthorized' })
+        }
     }
 }
 export default FilesController
